0b2d93a53990b01bf1e6331909b6772450cbf385
harshaonhub28 <hvk.2803@gmail.com>
2017-06-06 19:00:52 +0530

    video list done

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyDPf29ziU829iRoBk388vsyBQB-0pthtqM';
 
-const App = () => {
-  return (
-    <div>
-      Ho!
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'working class hero' }, data => {
+      this.setState({ videos: data });
+      console.log(this.state.videos);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        Ho!
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
