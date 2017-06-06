9c20888e21891b6253adcd7943780b7f283fd72c
harshaonhub28 <hvk.2803@gmail.com>
2017-06-06 16:37:20 +0530

    new components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyDPf29ziU829iRoBk388vsyBQB-0pthtqM';
+
 const App = () => {
-  return <div>Hola!</div>;
+  return (
+    <div>
+      Ho!
+      <SearchBar />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector('.container'));
